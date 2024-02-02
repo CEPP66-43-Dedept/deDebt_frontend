ab6c92ee41804dc7004050de8449df04560c80c6
Areeya Suwannathot <88705670+areeyaswnth@users.noreply.github.com>
2024-02-02 20:26:15 +0700

    feat(frontend) : login screen

--- dedebt_application/lib/screens/loginScreen.dart
+++ dedebt_application/lib/screens/loginScreen.dart
@@ -17,35 +17,164 @@ class _LoginScreenState extends State<LoginScreen> {
   }
 
   Widget build(BuildContext context) {
+    final boderInputStyle = OutlineInputBorder(
+        borderSide: BorderSide.none, borderRadius: BorderRadius.circular(12.0));
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       body: Container(
         height: MediaQuery.of(context).size.height,
-        decoration: const BoxDecoration(color: ColorGuide.whiteAccent),
-        child: ListView(
-          children: [
-            const SizedBox(height: 100),
-            Container(
-              decoration: BoxDecoration(
-                color: ColorGuide.white,
-                shape: BoxShape.circle,
-              ),
-              child: Padding(
-                padding: const EdgeInsets.symmetric(
-                  vertical: 25.0,
-                  horizontal: 60.0,
-                ),
-                child: Image.asset(
-                  "assets/images/Logo.png",
-                  width: 75,
-                ),
-              ),
-            ),
-            ElevatedButton(
-              onPressed: () => context.go(AppRoutes.INITIAL),
-              child: const Text('Go to the Details screen'),
-            ),
-          ],
+        width: MediaQuery.of(context).size.width,
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [
+                Color(0xFFF2F5FF),
+                Color(0xFFF2F5FF),
+                ColorGuide.whiteAccent,
+              ]),
         ),
+        child: Column(children: [
+          const SizedBox(height: 110),
+          Container(
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+            ),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(
+                vertical: 25.0,
+                horizontal: 60.0,
+              ),
+              child: Image.asset(
+                "assets/images/Logo.png",
+                width: 150,
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 40,
+          ),
+          Container(
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                boxShadow: [
+                  BoxShadow(
+                      color: Color.fromRGBO(54, 51, 140, 0.26),
+                      blurRadius: 5.0,
+                      blurStyle: BlurStyle.normal)
+                ],
+                color: ColorGuide.white),
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                children: [
+                  const Text(
+                    'เข้าสู่ระบบ',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 20,
+                      fontWeight: FontWeight.normal,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Column(children: [
+                    SizedBox(
+                      height: 45,
+                      width: 250,
+                      child: TextField(
+                        keyboardType: TextInputType.text,
+                        style: const TextStyle(
+                          color: Colors.black,
+                        ),
+                        decoration: InputDecoration(
+                            floatingLabelBehavior: FloatingLabelBehavior.never,
+                            hintText: "อีเมล",
+                            hintStyle: const TextStyle(
+                              color: ColorGuide.blueAccent,
+                              fontSize: 16,
+                            ),
+                            filled: true,
+                            fillColor: ColorGuide.blueLighten,
+                            focusedBorder: boderInputStyle,
+                            enabledBorder: boderInputStyle,
+                            contentPadding:
+                                const EdgeInsets.symmetric(horizontal: 20),
+                            prefixIcon: const Icon(Icons.account_circle),
+                            prefixIconColor: ColorGuide.blueAccent),
+                      ),
+                    ),
+                    const SizedBox(height: 25),
+                    SizedBox(
+                      height: 45,
+                      width: 250,
+                      child: TextField(
+                        obscureText: true,
+                        keyboardType: TextInputType.visiblePassword,
+                        style: const TextStyle(
+                          color: Colors.black,
+                        ),
+                        decoration: InputDecoration(
+                            floatingLabelBehavior: FloatingLabelBehavior.never,
+                            hintText: "รหัสผ่าน",
+                            hintStyle: const TextStyle(
+                              color: ColorGuide.blueAccent,
+                              fontSize: 16,
+                            ),
+                            filled: true,
+                            fillColor: ColorGuide.blueLighten,
+                            focusedBorder: boderInputStyle,
+                            enabledBorder: boderInputStyle,
+                            contentPadding:
+                                const EdgeInsets.symmetric(horizontal: 20),
+                            prefixIcon: const Icon(
+                              Icons.lock_rounded,
+                              color: ColorGuide.blueAccent,
+                            ),
+                            prefixIconColor: Colors.black),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    )
+                  ]),
+                ],
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 70,
+          ),
+          ElevatedButton(
+            style: ButtonStyle(
+              backgroundColor: MaterialStateProperty.all(ColorGuide.blueAccent),
+              shadowColor: MaterialStateProperty.all(ColorGuide.black),
+            ),
+            onPressed: () => context.go(AppRoutes.INITIAL),
+            child: const Text(
+              'เข้าสู่ระบบ',
+              style: TextStyle(color: ColorGuide.blueLight, fontSize: 16),
+            ),
+          ),
+          const SizedBox(height: 10),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text('ยังไม่มีบัญชีผู้ใช้?'),
+              const SizedBox(width: 4),
+              GestureDetector(
+                child: const Text(
+                  'ลงทะเบียน',
+                  style: TextStyle(
+                    decoration: TextDecoration.underline,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ]),
       ),
     );
   }
